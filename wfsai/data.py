533da0e559eba984990f1a28dbf3459f67e2171e
matscorse <matsco@bas.ac.uk>
2025-04-11 10:55:21 +0100

    fixed incorrect import reference

--- wfsai/data.py
+++ wfsai/data.py
@@ -10,14 +10,16 @@ import shutil
 import yaml
 import git
 from pathlib import Path
-from configuration import _check_config_path_
-from configuration import _load_
+from wfsai.configuration import _check_config_path_
+from wfsai.configuration import _load_
 
 
 def retrieve(directory_path: str, config_file: str, data_type: str) -> None:
     """
-    Retrieve a specific data type according to specifics set out in the yaml config file.
-    The retrieved data will be stored in the location specified in the yaml config file.
+    Retrieve a specific data type according to specifics in the
+    yaml config file.
+    The retrieved data will be stored in the location specified
+    in the yaml config file.
     This method always returns None.
     """
     yaml_path = Path.joinpath(Path(str(directory_path)), str(config_file))
