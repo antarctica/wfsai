48656ce9a6e411e852378552c5c40b69e0b698dc
matscorse <matsco@bas.ac.uk>
2025-04-11 11:13:49 +0100

    ported retrieval of annotations into data class

--- wfsai/data.py
+++ wfsai/data.py
@@ -8,7 +8,7 @@ import sys
 import os
 import shutil
 import yaml
-import git
+import glob
 from pathlib import Path
 from wfsai.configuration import _check_config_path_
 from wfsai.configuration import _load_
@@ -27,7 +27,26 @@ def retrieve(directory_path: str, config_file: str, data_type: str) -> None:
     if _check_config_path_(yaml_path):
         data_yaml = load(yaml_path)[str(data_type)]
 
-        for e in data_yaml:
-            pass
+        for dir_group in data_yaml:
+            # Do specified source and destination directories exist
+            if dir_group['source_dir'] is not None and dir_group['dest_dir'] is not None:
+                if (not os.path.isdir(dir_group['source_dir'])) or (not os.path.isdir(dir_group['dest_dir'])):
+                    print("Source or destination directory not found")
+                    exit(1)
+
+            # For each specified file, copy or link to destination
+            for e in dir_group['sources']:
+                # Check the source directory exists
+                if os.path.isdir(Path.joinpath(Path(dir_group['source_dir']), e['dir'])):
+                    src_directory = Path.joinpath(Path(dir_group['source_dir']), e['dir'])
+                    for file in e['files']:
+                        # Expand out any wildcards, i.e. *.*
+                        for subfile in glob.glob(str(Path.joinpath(src_directory, file))):
+                            # Make sure the source files each exist
+                            if os.path.isfile(Path.joinpath(src_directory, os.path.basename(subfile))):
+                                dest_directory = Path(dir_group['dest_dir'])
+                                # Only copy if file isn't already in destination directory
+                                if not os.path.isfile(Path.joinpath(dest_directory, os.path.basename(subfile))):
+                                    shutil.copy(subfile, Path.joinpath(dest_directory, os.path.basename(subfile)))
     
     return None
