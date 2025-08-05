03f0c4b66c6f1d8e90a10683adedbd75d089b10d
matscorse <matsco@bas.ac.uk>
2025-08-05 16:38:36 +0100

    initial commit

---
+++ wfsai/execution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# File Version: 2025-08-05: First version
+#
+# Author: matsco@bas.ac.uk
+
+import sys
+import os
+import shutil
+import yaml
+import git
+from pathlib import Path
+from typing import Union
+
+
+ 
